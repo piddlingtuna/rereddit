a73da1a8804dede5d1987e781a0328e3e361a0d0
Michael Gribben <michaelgribbeninfo@gmail.com>
2020-02-09 22:00:05 +1100

    added temp sub

--- frontend/src/views/s.vue
+++ frontend/src/views/s.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    
+    <div>
+      <div class="hero is-info">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">
+              {{ sub }}
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      This section is incomplete.
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'subPage',
+  props: {
+    sub: String
+  }
+}
+</script>
